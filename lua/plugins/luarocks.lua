ec3b3b7cc5f9aaea04d727d8af3e1cc6db414d7b
lam <therecipeofholywater@gmail.com>
2024-05-06 07:08:46 +0700

    merge luarocks and neorg to one file

--- lua/plugins/luarocks.lua
+++ lua/plugins/luarocks.lua
@@ -4,4 +4,12 @@ return {
     priority = 1000,
     config = true,
   },
+
+  {
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    config = true,
+  },
 }
