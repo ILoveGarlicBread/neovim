5f5d5abb38d6d33b1ce40c480acf84266a482cbe
lam <therecipeofholywater@gmail.com>
2024-05-30 09:32:08 +0700

    added autopairs and magma
    magma need fixing

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,8 @@
+-- Prevent adding unnecessary brackets
+return {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+}
