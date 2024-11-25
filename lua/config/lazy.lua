092e6d41f4ffb6ae40dab5bd22bc13443b5a119b
lam <therecipeofholywater@gmail.com>
2024-11-25 09:03:49 +0700

    enable luasnip

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     --{ import = "lazyvim.plugins.extras.formatting.black" },
+    { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.lang.tex" },
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.dap.core" },
